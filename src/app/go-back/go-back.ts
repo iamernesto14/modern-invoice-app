9391bd30868478f1bfd0ec635ddf17da7db22b75
iamernesto14 <eernesto211@gmail.com>
2025-06-05 04:45:17 +0000

    initialize various components

---
+++ src/app/go-back/go-back.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-go-back',
+  imports: [],
+  templateUrl: './go-back.html',
+  styleUrl: './go-back.css'
+})
+export class GoBack {
+
+}
