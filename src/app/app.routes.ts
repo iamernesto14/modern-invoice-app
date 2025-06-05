7647269e40dd97fe98c6eb9ce4afe4b1c3a28e59
iamernesto14 <eernesto211@gmail.com>
2025-06-05 07:02:38 +0000

    Add Routing for various pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// local imports
+import { InvoicePage } from './pages/invoice-page/invoice-page';
+import { InvoiceDetailPage } from './pages/invoice-detail-page/invoice-detail-page';
+import { InvoiceForm } from './components/invoice-form/invoice-form';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: InvoicePage,
+        title: 'Invoice',
+    },
+    {
+        path: '',
+        children: [
+            {
+                path: 'new-form',
+                component: InvoiceForm,
+            },
+            
+        ]
+    },
+    {
+        path: 'invoice',
+        component: InvoiceDetailPage,
+        title: 'Invoice',
+    },
+    {
+        path: 'invoice',
+        children: [
+            {
+                path: 'edit-form',
+                component: InvoiceForm,
+            },
+        ]
+    }
+];
