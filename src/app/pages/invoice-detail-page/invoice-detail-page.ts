831de5cce4251041635235f87843dc5a423bd762
iamernesto14 <eernesto211@gmail.com>
2025-06-05 04:52:45 +0000

    initialize invoice page and detail invoice page

---
+++ src/app/pages/invoice-detail-page/invoice-detail-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-detail-page',
+  imports: [],
+  templateUrl: './invoice-detail-page.html',
+  styleUrl: './invoice-detail-page.css'
+})
+export class InvoiceDetailPage {
+
+}
